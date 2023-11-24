65b144681b5be52a431982a2f1b91c286459f6fc
Charlie025x <charliealonso2001@gmail.com>
2023-11-23 21:34:38 -0600

    worked on css to get closer to mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -5,60 +5,97 @@ function App() {
         <nav className="flex justify-between p-4">
           <img src="./assets/images/logo.svg" alt="" />
           <img src="./assets/images/icon-menu.svg" alt="" />
-
         </nav>
-        <div>
-          <div><img src="./assets/images/image-web-3-mobile.jpg" alt="" /></div>
-        </div>
-        <div>
-          <h1 className=" font-bold text-5xl">The Bright Future of Web 3.0?</h1>
-        </div>
-        <div>
-          <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
-          <button className="text-white bg-orange-600 p-3 py-1">Read more</button>
-        </div>
 
-        <div className=" bg-slate-950 text-white">
-
-         <h2>New</h2>
-         <div>
-           <h3>Hydrogen VS Electric Cars</h3>
-           <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-         </div>
-         <div>
-           <h3>The Downsides of AI Artistry</h3>
-           <p>What are the possible adverse effects of on-demand AI image generation?</p>
-         </div>
-         <div>
-           <h3>Is VC Funding Drying Up?</h3>
-           <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
-          </div>
-
-         </div>
-         
-        <div className="">
-          
+        <div className="mx-4 grid justify-center gap-4 lg:grid-cols-3">
           <div>
-            <img src="./assets/images/image-retro-pcs.jpg" alt="" />
-          <h2>01</h2>
-            <h3>Reviving Retro PCs</h3>
-            <p>What happens when old PCs are given modern upgrades?</p>
+            <img src="./assets/images/image-web-3-mobile.jpg" alt="" />
           </div>
           <div>
-            <img src="./assets/images/image-top-laptops.jpg" alt="" />
-          <h2>02</h2>
-            <h3>Top 10 Laptops of 2022</h3>
-            <p>Our best picks for various needs and budgets.</p>
+            <h1 className=" text-5xl font-bold">
+              The Bright Future of Web 3.0?
+            </h1>
           </div>
           <div>
-            <img src="./assets/images/image-gaming-growth.jpg" alt="" />
-          <h2>03</h2>
-            <h3>The Growth of Gaming</h3>
-            <p>How the pandemic has sparked fresh opportunities.</p>
-           </div>
- 
+            <p className=" mb-4">
+              We dive into the next evolution of the web that claims to put the
+              power of the platforms back into the hands of the people. But is
+              it really fulfilling its promise?
+            </p>
+            <button className="mb-4 bg-orange-600 p-3 py-1 text-white">
+              Read more
+            </button>
           </div>
 
+          <div className=" bg-slate-950 p-4 text-white">
+            <h2 className="text-4xl text-orange-300">New</h2>
+            <div className="border-b py-4">
+              <h3 className="mb-2 text-xl font-bold">
+                Hydrogen VS Electric Cars
+              </h3>
+              <p className="mb-2 text-gray-500">
+                Will hydrogen-fueled cars ever catch up to EVs?
+              </p>
+            </div>
+            <div className="border-b py-4">
+              <h3 className="mb-2 text-xl font-bold">
+                The Downsides of AI Artistry
+              </h3>
+              <p className="mb-2 text-gray-500">
+                What are the possible adverse effects of on-demand AI image
+                generation?
+              </p>
+            </div>
+            <div className="py-4 pb-0">
+              <h3 className="mb-2 text-xl font-bold">
+                Is VC Funding Drying Up?
+              </h3>
+              <p className="mb-2 text-gray-500">
+                Private funding by VC firms is down 50% YOY. We take a look at
+                what that means.
+              </p>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="grid grid-cols-2 gap-4">
+              <img
+                className=" row-span-3"
+                src="./assets/images/image-retro-pcs.jpg"
+                alt=""
+              />
+              <h2 className="text-xl font-bold text-gray-500">01</h2>
+              <h3>Reviving Retro PCs</h3>
+              <p className="text-gray-500">
+                What happens when old PCs are given modern upgrades?
+              </p>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <img
+                className=" row-span-3"
+                src="./assets/images/image-top-laptops.jpg"
+                alt=""
+              />
+              <h2 className="text-xl font-bold text-gray-500">02</h2>
+              <h3>Top 10 Laptops of 2022</h3>
+              <p className="text-gray-500">
+                Our best picks for various needs and budgets.
+              </p>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <img
+                className=" row-span-3"
+                src="./assets/images/image-gaming-growth.jpg"
+                alt=""
+              />
+              <h2 className="text-xl font-bold text-gray-500">03</h2>
+              <h3>The Growth of Gaming</h3>
+              <p className="text-gray-500">
+                How the pandemic has sparked fresh opportunities.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
